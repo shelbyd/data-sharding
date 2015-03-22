2239bfa5d62ecff8d8acfa30c41181bb9dca3481
Shelby Doolittle <shelby@shelbyd.com>
2015-03-22 09:56:46 -0700

    remove result caching

--- main.rb
+++ main.rb
@@ -18,9 +18,6 @@ def run_with_possibilities(possibilities)
     max_column_count = [max_column_count, possibility.column_count].max
     moving_average_remaining = (moving_average_remaining * 99999 + possibilities.size) / 100000
     if count % 1000 == 0
-      if count % 10000 == 0
-        File.write('./cache.yaml', YAML.dump(possibilities))
-      end
       puts "Have a #{possibility.row_count} by #{possibility.column_count} possibility"
       puts "#{moving_average_remaining.round(2)} possibilities remaining"
       puts "Max width #{max_column_count}"
@@ -39,8 +36,12 @@ def run_with_possibilities(possibilities)
   end
 end
 
-if File.exists?('./cache.yaml')
-  run_with_possibilities(YAML.load(File.read('./cache.yaml')))
-else
-  path_to(Matrix[ *([[1]] * 7) ])
-end
+path_to(Matrix[ *([[1]] * 11) ])
+
+#  6 420
+#  7 36
+#  8 24
+#  9 24
+# 10 21 (24?)
+# 11 21 (24?)
+# 12 12
